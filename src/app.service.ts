c7e2cb0fded2d8300d96c92696617a22fd8d48f9
Ezra Lazuardy <ezralazuardy@icloud.com>
2024-07-02 18:33:24 +0700

    feat: update

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { File } from '@nest-lab/fastify-multer';
-import * as AWS from 'aws-sdk';
+import { Upload } from '@aws-sdk/lib-storage';
+import { S3, ObjectCannedACL } from '@aws-sdk/client-s3';
 
 @Injectable()
 export class AppService {
-  private readonly s3: AWS.S3;
+  private readonly s3: S3;
 
   constructor() {
-    this.s3 = new AWS.S3({
-      accessKeyId: process.env.R2_ACCESS_KEY_ID,
-      secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+    this.s3 = new S3({
+      credentials: {
+        accessKeyId: process.env.R2_ACCESS_KEY_ID,
+        secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+      },
+
       endpoint: process.env.R2_ENDPOINT,
     });
   }
@@ -20,13 +24,16 @@ export class AppService {
       Bucket: bucketName ?? process.env.R2_BUCKET_NAME,
       Key: file.originalname,
       Body: file.buffer,
-      ACL: 'public-read',
+      ACL: ObjectCannedACL.public_read,
       ContentType: file.mimetype,
       ContentDisposition: 'inline',
     };
 
     try {
-      const response = await this.s3.upload(params).promise();
+      const response = await new Upload({
+        client: this.s3,
+        params,
+      }).done();
       return response;
     } catch (error) {
       console.log(error);
